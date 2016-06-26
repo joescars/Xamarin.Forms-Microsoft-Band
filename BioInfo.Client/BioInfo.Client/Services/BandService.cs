08cd5990fe24ba53391ba923deac45afba4bd9ec
Joe Raio <Joe.Raio@Microsoft.com>
2016-06-26 12:27:33 -0400

    Code Cleanup. MVVM Implemented

---
+++ BioInfo.Client/BioInfo.Client/Services/BandService.cs
@@ -0,0 +1,153 @@
+﻿using BioInfo.Client.ValueConverters;
+using Microsoft.Band.Portable;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioInfo.Client.Services
+{
+    public class BandService : INotifyPropertyChanged
+    {
+        private BandClient bandClient;
+        private BandDeviceInfo band;
+        private BandClientManager bandClientManager;
+
+        private string currentSkinTemp = "Nothing to see here";
+        public string CurrentSkinTemp
+        {
+            get { return currentSkinTemp; }
+            set
+            {
+                currentSkinTemp = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string currentAmbientLight;
+        public string CurrentAmbientLight
+        {
+            get { return currentAmbientLight; }
+            set
+            {
+                currentAmbientLight = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string currentHeartRate;
+        public string CurrentHeartRate
+        {
+            get { return currentHeartRate; }
+            set
+            {
+                currentHeartRate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BandService()
+        {
+            bandClientManager = BandClientManager.Instance;
+        }
+        public async Task<IEnumerable<BandDeviceInfo>> getBands()
+        {            
+            var bands = await bandClientManager.GetPairedBandsAsync();
+            return bands;
+        }
+
+        public async Task<String> ConnectToBand(BandDeviceInfo b)
+        {
+            band = b;
+            bandClient = await bandClientManager.ConnectAsync(band);
+            return String.Format("connected to {0} !", band.Name);            
+        }
+
+        public async Task StartReadingSkinTemp()
+        {
+            await bandClient.SensorManager.SkinTemperature.StartReadingsAsync();
+            bandClient.SensorManager.SkinTemperature.ReadingChanged += SkinTemperature_ReadingChanged;
+        }
+        public async Task StopReadingSkinTemp()
+        {
+            await bandClient.SensorManager.SkinTemperature.StopReadingsAsync();
+            bandClient.SensorManager.SkinTemperature.ReadingChanged -= SkinTemperature_ReadingChanged;
+        }
+        private async void SkinTemperature_ReadingChanged(object sender, Microsoft.Band.Portable.Sensors.BandSensorReadingEventArgs<Microsoft.Band.Portable.Sensors.BandSkinTemperatureReading> e)
+        {
+            await Task.Run(() => {
+                this.CurrentSkinTemp = CelsiusToFahrenheitConverter.Convert(e.SensorReading.Temperature).ToString();
+            });
+        }
+        
+        public async Task StartReadingAmbientLight()
+        {
+            await bandClient.SensorManager.AmbientLight.StartReadingsAsync();
+            bandClient.SensorManager.AmbientLight.ReadingChanged += AmbientLight_ReadingChanged;
+        }
+        public async Task StopReadingAmbientLight()
+        {
+            await bandClient.SensorManager.AmbientLight.StopReadingsAsync();
+            bandClient.SensorManager.AmbientLight.ReadingChanged -= AmbientLight_ReadingChanged;
+        }
+        private async void AmbientLight_ReadingChanged(object sender, Microsoft.Band.Portable.Sensors.BandSensorReadingEventArgs<Microsoft.Band.Portable.Sensors.BandAmbientLightReading> e)
+        {
+            await Task.Run(() =>
+            {
+                this.CurrentAmbientLight = e.SensorReading.Brightness.ToString();
+            });
+        }
+
+        public async Task StartReadingHeartRate()
+        {
+            bool hrConsentGranted;
+            switch (bandClient.SensorManager.HeartRate.UserConsented)
+            {
+                case UserConsent.Declined:
+                    hrConsentGranted = false;
+                    break;
+
+                case UserConsent.Granted:
+                    hrConsentGranted = true;
+                    break;
+
+                default:
+                case UserConsent.Unspecified:
+                    hrConsentGranted = await bandClient.SensorManager.HeartRate.RequestUserConsent();
+                    break;
+            }
+            if (hrConsentGranted)
+            {
+                await bandClient.SensorManager.HeartRate.StartReadingsAsync();
+                bandClient.SensorManager.HeartRate.ReadingChanged += HeartRate_ReadingChanged;
+            }
+            else
+            {
+                this.CurrentHeartRate = "No Consent for HR";
+            }
+
+        }
+        public async Task StopReadingHeartRate()
+        {
+            await bandClient.SensorManager.HeartRate.StopReadingsAsync();
+            bandClient.SensorManager.HeartRate.ReadingChanged -= HeartRate_ReadingChanged;
+        }
+        private async void HeartRate_ReadingChanged(object sender, Microsoft.Band.Portable.Sensors.BandSensorReadingEventArgs<Microsoft.Band.Portable.Sensors.BandHeartRateReading> e)
+        {
+            await Task.Run(() =>
+            {
+                this.CurrentHeartRate = e.SensorReading.HeartRate.ToString();
+            });
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ BioInfo.Client/BioInfo.Client/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BioInfo.Client.ValueConverters;
+using Microsoft.Band.Portable;
+using Xamarin.Forms;
+using BioInfo.Client.Services;
+
+namespace BioInfo.Client.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Band Setup
+
+        //local band that we will connect to
+        private BandDeviceInfo band;
+
+        private BandService bandService;
+
+        // Interface
+
+        private string pageTitle;
+        private string currentStatus;
+        private string connectButtonText;
+        private bool connectButtonEnabled;
+
+        // Sensor Stats
+
+        private string readHR;
+        private string readHRQuality;
+        private string readSkinTemp;
+        private string readAmbientLight;
+        private bool isToggleSkinTemp;
+        private bool isToggleAmbientLight;
+        private bool isToggleHeartRate;
+
+        public bool IsToggleSkinTemp
+        {
+            get { return isToggleSkinTemp; }
+            set
+            {
+                if (isToggleSkinTemp != value)
+                {
+                    isToggleSkinTemp = value;
+                    OnPropertyChanged();
+                    ToggleSkinTemp();
+                }
+            }
+        }
+
+        public bool IsToggleAmbientLight
+        {
+            get { return isToggleAmbientLight; }
+            set
+            {
+                if (isToggleAmbientLight != value)
+                {
+                    isToggleAmbientLight = value;
+                    OnPropertyChanged();
+                    ToggleAmbientLight();
+                }
+            }
+        }
+
+        public bool IsToggleHeartRate
+        {
+            get { return isToggleHeartRate; }
+            set
+            {
+                if (isToggleHeartRate != value)
+                {
+                    isToggleHeartRate = value;
+                    OnPropertyChanged();
+                    ToggleHeartRate();
+                }
+            }
+        }
+
+        public string PageTitle
+        {
+            get { return pageTitle; }
+            set
+            {
+                pageTitle = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string CurrentStatus
+        {
+            get { return currentStatus; }
+            set
+            {
+                currentStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ConnectButtonText
+        {
+            get { return connectButtonText; }
+            set
+            {
+                connectButtonText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool ConnectButtonEnabled
+        {
+            get { return connectButtonEnabled; } 
+            set
+            {
+                connectButtonEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ReadHR
+        {
+            get { return readHR; }
+            set
+            {
+                readHR = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ReadHRQuality
+        {
+            get { return readHRQuality; }
+            set
+            {
+                readHRQuality = value;
+                OnPropertyChanged();
+            }
+        }
+        public string ReadSkinTemp
+        {
+            get { return readSkinTemp; }
+            set
+            {
+                readSkinTemp = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ReadAmbientLight
+        {
+            get { return readAmbientLight; }
+            set
+            {
+                readAmbientLight = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            pageTitle = "BioInformatics";
+            currentStatus = "Current Status";
+            connectButtonText = "Connect to Band";
+            readHR = "25";
+            readSkinTemp = "Not Connected";
+            readAmbientLight = "Not Connected";
+            isToggleSkinTemp = false;
+            isToggleAmbientLight = false;
+            connectButtonEnabled = true;           
+
+            bandService = new BandService();           
+
+            getBands();
+        }
+        
+        private async void getBands()
+        {
+            var bands = await bandService.getBands();
+            band = bands.FirstOrDefault();
+            if (band == null)
+            {
+                this.CurrentStatus = "tried but failed";
+                return;
+            }
+            this.ConnectButtonText = "Connect to Band: " + band.Name;
+        }
+
+        private async void ConnectToBand()
+        {
+            this.CurrentStatus = "Connecting...";
+            var result = await bandService.ConnectToBand(band);             
+            this.CurrentStatus = result;
+            this.ConnectButtonEnabled = false;
+            bandService.PropertyChanged += BandService_PropertyChanged;
+            //EnableToggles();
+        }
+
+        private async void BandService_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                this.ReadSkinTemp = bandService.CurrentSkinTemp;
+                this.ReadAmbientLight = bandService.CurrentAmbientLight;
+                this.ReadHR = bandService.CurrentHeartRate;
+            });            
+        }
+
+        public Command Connect {
+            get
+            {
+                return new Command(() =>
+                {
+                    ConnectToBand();
+                });
+            }                
+        }
+
+        // Band Readings
+        private async void ToggleSkinTemp()
+        {
+            if (isToggleSkinTemp)
+                await bandService.StartReadingSkinTemp();
+            else
+                await bandService.StopReadingSkinTemp();
+            
+        }
+        private async void ToggleAmbientLight()
+        {
+            if (isToggleAmbientLight)
+                await bandService.StartReadingAmbientLight();
+            else
+                await bandService.StopReadingAmbientLight();
+        }
+
+        private async void ToggleHeartRate()
+        {
+            if (isToggleHeartRate)
+                await bandService.StartReadingHeartRate();
+            else
+                await bandService.StartReadingHeartRate();
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
